8cfcda545a5a7e1da10fc85748ef7ab893a5caf0
Артем <artikus.sol@gmail.com>
2024-06-11 00:12:57 +0400

    refactor(bxbw): поддержка php 8.1 (начало)

--- classes/Boxberry/src/Collections/Collection.php
+++ classes/Boxberry/src/Collections/Collection.php
@@ -16,135 +16,165 @@
 
 namespace Boxberry\Collections;
 
+
 /**
  * Абстрактная коллекция, служащая базой.
  * Class Collection
+ *
  * @package Boxberry\Collections
  */
-abstract class Collection implements \ArrayAccess, \Countable, \Iterator, \Serializable
-{
-    /**
-     * @var array
-     */
-    protected $_container = array();
-    /**
-     * @var int
-     */
-    protected $_position = 0;
+abstract class Collection implements \ArrayAccess, \Countable, \Iterator, \Serializable {
 
-    /**
-     * Collection constructor.
-     * @param $data
-     */
-    abstract function __construct($data);
+	/**
+	 * @var array
+	 */
+	protected $_container = [];
 
-    /**
-     * @param mixed $offset
-     * @return bool
-     */
-    public function offsetExists($offset)
-    {
-        return isset($this->_container[$offset]);
-    }
 
-    /**
-     * @param mixed $offset
-     * @return null
-     */
-    public function offsetGet($offset)
-    {
-        return $this->offsetExists($offset) ? $this->_container[$offset] : null;
-    }
+	/**
+	 * @var int
+	 */
+	protected $_position = 0;
 
-    /**
-     * @param mixed $offset
-     */
-    public function offsetUnset($offset)
-    {
-        unset($this->_container[$offset]);
-    }
 
-    /**
-     *
-     */
-    public function rewind()
-    {
-        $this->_position = 0;
-    }
+	/**
+	 * Collection constructor.
+	 *
+	 * @param $data
+	 */
+	abstract function __construct( $data );
 
-    /**
-     * @return mixed
-     */
-    public function current()
-    {
-        return $this->_container[$this->_position];
-    }
 
-    /**
-     * @return int
-     */
-    public function key()
-    {
-        return $this->_position;
-    }
+	/**
+	 * @param  mixed $offset
+	 *
+	 * @return bool
+	 */
+	public function offsetExists( $offset ): bool {
 
-    /**
-     *
-     */
-    public function next()
-    {
-        ++$this->_position;
-    }
+		return isset( $this->_container[ $offset ] );
+	}
 
-    /**
-     * @return bool
-     */
-    public function valid()
-    {
-        return isset($this->_container[$this->_position]);
-    }
 
-    /**
-     * @return int
-     */
-    public function count()
-    {
-        return count($this->_container);
-    }
+	/**
+	 * @param  mixed $offset
+	 *
+	 * @return null
+	 */
+	#[\ReturnTypeWillChange]
+	public function offsetGet( $offset ) {
 
-    /**
-     * @return string
-     */
-    public function serialize()
-    {
-        return serialize($this->_container);
-    }
+		return $this->offsetExists( $offset ) ? $this->_container[ $offset ] : null;
+	}
 
-    /**
-     * @param string $data
-     */
-    public function unserialize($data)
-    {
-        $this->_container = unserialize($data);
-    }
 
-    /**
-     * @param array|null $data
-     * @return array
-     */
-    public function __invoke(array $data = null)
-    {
-        if (!is_null($data)) {
-            $this->_container = $data;
-        }
-        return $this->_container;
-    }
+	/**
+	 * @param  mixed $offset
+	 */
+	#[\ReturnTypeWillChange]
+	public function offsetUnset( $offset ) {
 
-    /**
-     * @return \ArrayIterator
-     */
-    public function getIterator()
-    {
-        return new \ArrayIterator($this);
-    }
+		unset( $this->_container[ $offset ] );
+	}
+
+
+	/**
+	 *
+	 */
+	#[\ReturnTypeWillChange]
+	public function rewind() {
+
+		$this->_position = 0;
+	}
+
+
+	/**
+	 * @return mixed
+	 *
+	 */
+	#[\ReturnTypeWillChange]
+	public function current() {
+
+		return $this->_container[ $this->_position ];
+	}
+
+
+	/**
+	 * @return int
+	 */
+	public function key(): int {
+
+		return $this->_position;
+	}
+
+
+	/**
+	 *
+	 */
+	#[\ReturnTypeWillChange]
+	public function next() {
+
+		++ $this->_position;
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	public function valid(): bool {
+
+		return isset( $this->_container[ $this->_position ] );
+	}
+
+
+	/**
+	 * @return int
+	 */
+	public function count(): int {
+
+		return count( $this->_container );
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function serialize(): string {
+
+		return serialize( $this->_container );
+	}
+
+
+	/**
+	 * @param  string $data
+	 */
+	#[\ReturnTypeWillChange]
+	public function unserialize( $data ) {
+
+		$this->_container = unserialize( $data );
+	}
+
+
+	/**
+	 * @param  array|null $data
+	 *
+	 * @return array
+	 */
+	public function __invoke( array $data = null ): array {
+
+		if ( ! is_null( $data ) ) {
+			$this->_container = $data;
+		}
+
+		return $this->_container;
+	}
+
+
+	/**
+	 * @return \ArrayIterator
+	 */
+	public function getIterator(): \ArrayIterator {
+
+		return new \ArrayIterator( $this );
+	}
 }
